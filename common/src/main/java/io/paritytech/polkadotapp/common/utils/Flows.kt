ff6b3f774c1e5e36b6193db357fadcebc942862b
valentunn <vallentunn@gmail.com>
2026-09-18 15:14:29 +0300

    Gate incoming balance updates after transfer has started

--- common/src/main/java/io/paritytech/polkadotapp/common/utils/Flows.kt
+++ common/src/main/java/io/paritytech/polkadotapp/common/utils/Flows.kt
@@ -164,6 +164,10 @@ fun <T1, T2, T3> combineToTriple(
 ): Flow<Triple<T1, T2, T3>> =
     combine(flow1, flow2, flow3, ::Triple)
 
+fun <T> Flow<T>.gate(flag: Flow<Boolean>) = combineToPair(this, flag)
+    .filter { it.second }
+    .map { it.first }
+
 fun <T> Flow<T>.zipWithPrevious(): Flow<Pair<T?, T>> =
     flow {
         var current: T? = null
