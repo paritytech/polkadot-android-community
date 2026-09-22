4ad482823721afffdd8ffb9383b73a5a21bfd802
Muhammad Subhan <subhan@parity.io>
2026-09-22 16:06:39 +0500

    Hide the info icon on the balance row

--- feature/wallet/impl/src/main/java/io/paritytech/polkadotapp/feature_wallet_impl/presentation/enterAmount/compose/components/EnterAmountBalance.kt
+++ feature/wallet/impl/src/main/java/io/paritytech/polkadotapp/feature_wallet_impl/presentation/enterAmount/compose/components/EnterAmountBalance.kt
@@ -4,24 +4,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import io.paritytech.polkadotapp.design.components.icon.NovaIcon
-import io.paritytech.polkadotapp.design.components.icon.NovaIcons
-import io.paritytech.polkadotapp.design.components.icon.vectors.Info
 import io.paritytech.polkadotapp.design.components.spacer.HorizontalSpacer
 import io.paritytech.polkadotapp.design.components.spacer.VerticalSpacer
 import io.paritytech.polkadotapp.design.components.text.NovaText
 import io.paritytech.polkadotapp.design.theme.PolkadotTheme
 import io.paritytech.polkadotapp.common.R as RCommon
-
-private val InfoIconSize = 20.dp
 
 /**
  * [gainingPrivacy] is named on its own line rather than added to the balance: it is spendable, but only at
@@ -51,18 +44,9 @@ internal fun EnterAmountBalance(
             HorizontalSpacer { extraSmall }
 
             NovaText(
-                modifier = Modifier.weight(1f, fill = false),
                 text = stringResource(RCommon.string.send_enter_amount_ready_to_send),
                 style = PolkadotTheme.typography.body.large,
                 color = PolkadotTheme.colors.fg.secondary
-            )
-
-            HorizontalSpacer { extraSmall }
-
-            NovaIcon(
-                modifier = Modifier.size(InfoIconSize),
-                imageVector = NovaIcons.Info,
-                tint = PolkadotTheme.colors.fg.secondary
             )
         }
 
